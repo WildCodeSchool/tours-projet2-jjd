032a2a5674d679fe5f4c51fc4f46b126a3aabc9b
Jérémy Foucault <jeremfouc@orange.fr>
2019-04-02 16:52:37 +0200

    création component body navbar et footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 
+
 const routes: Routes = [
-  {path: '', component: HomeComponent};
+  {path: '', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HomeComponent } from './component/home/home.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { BodyComponent } from './component/body/body.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
