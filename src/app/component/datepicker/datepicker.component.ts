2301d77b34639b39aa759d24221a8654c28f02a7
jocelyn <jojoe45@hotmail.fr>
2019-04-04 15:46:31 +0200

    Responsive card

--- src/app/component/datepicker/datepicker.component.ts
+++ src/app/component/datepicker/datepicker.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngbd-datepicker-popup',
   templateUrl: './datepicker.component.html',
+  styleUrls: ['./datepicker.component.css'],
 })
 export class NgbdDatepickerPopup {
   model;
