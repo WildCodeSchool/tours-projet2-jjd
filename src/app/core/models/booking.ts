df7d215a056ffb8ef7d1fad358038dd903937599
Dylan Le Hir <lehir-dylan@hotmail.com>
2019-04-10 09:42:54 +0200

    Model terminée

--- src/app/core/models/booking.ts
+++ src/app/core/models/booking.ts
@@ -1,2 +1,30 @@
 export class Booking {
-}
+    constructor(
+        public date: {
+            am: {
+                start: number,
+                end: number,
+            },
+            pm: {
+                start: number,
+                end: number,
+            },
+        },
+
+        public owner: {
+            name: string,
+            adress: {
+                street: string,
+                zipCode: string, 
+                city: string, 
+                number: number,
+            },
+            contact: {
+                phone: string,
+                email: string,
+            },
+        },
+        public numbers: number,
+        public establishment: string,
+    ) {}
+};
