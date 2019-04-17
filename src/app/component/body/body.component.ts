6d2b578b3a00169808eee0a9fb5b299b7952d3d9
Jérémy Foucault <jeremfouc@orange.fr>
2019-04-17 16:22:25 +0200

    modification home page + nav bar + ajout route home sur login

--- src/app/component/body/body.component.ts
+++ src/app/component/body/body.component.ts
@@ -4,10 +4,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css'],
 })
+
 export class BodyComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
+
 }
