4cd5c7349fd5aac40d4957d00054c3ae1ba31dd7
jocelyn-Florance <jojoe45@hotmail.fr>
2019-04-29 12:43:53 +0200

    ProfileService duplicate code Issues #33

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -20,14 +20,6 @@ export class ProfileService {
     return recup.pipe(map(treatment));
   }
 
-  public getListProfile():Observable<Profile> {
-    const recup: Observable<any> = this.http.get(`${this.configUrl}`);
-    const treatment = (parameters:any) => {
-      return parameters as Profile;
-    };
-    return recup.pipe(map(treatment));
-  }
-
   public putProfile(profileForm: any):Observable<Profile> {
     const update: Observable<any> = this.http.put(`${this.configUrl}`, profileForm);
     const treatment = (response:any) => {
